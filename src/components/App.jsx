654dc2978e9316e717e8a532e3643da2b170f7b3
K-Basket <koshykkv@gmail.com>
2025-04-29 19:09:07 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        paddingTop: '100px',
+        // alignItems: 'center',
+        fontSize: 15,
+        color: '#010101',
+      }}
+    >
+      <h1>Template project by K-Basket</h1>
+    </div>
+  );
+};
